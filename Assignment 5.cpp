a9771a68a1e394a6586b2f773fb6dccc24290ed8
Ambuj Kumar Singh <ambujks2002@gmail.com>
2024-08-18 23:46:54 +0530

    Update Assignment 5.cpp

--- Assignment 5.cpp
+++ Assignment 5.cpp
@@ -3,11 +3,6 @@ Write a C++ program that incorporates the concept you’ve learned in recent cla
 
 Note: Specify any new classes, methods, or features you implement. Use comments to explain the purpose and functionality of your additions.
 */
-/*
-Write a C++ program that incorporates the concept you’ve learned in recent classes. You may take inspiration from the C++ Bank Management System project covered on Project Day.
-
-Note: Specify any new classes, methods, or features you implement. Use comments to explain the purpose and functionality of your additions.
-*/
 
 #include<iostream>
 using namespace std;
